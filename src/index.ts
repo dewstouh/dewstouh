3d545c891b73c8a5e07f7e1d65bd223ecb9cfb54
dewstouh <diego@justdiego.com>
2025-07-04 12:46:03 +0200

    update: remove top starred repos and tech stack

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { getLatestYoutubeVideos } from "./services/youtube.service";
+// import { getLatestYoutubeVideos } from "./services/youtube.service";
 import { promises as fs } from 'fs'
 import { format } from 'date-fns'
-import { ACTIVITY_PLACEHOLDER, DATE_PLACEHOLDER, GITHUB_USERNAME, ORGANIZATIONS_PLACEHOLDER, STARS_PLACEHOLDER, YOUTUBE_PLACEHOLDER } from './config';
-import { generateYoutubeHTML } from './utils/generateYoutubeHTML';
-import { getRecentActivity, getTopStarredRepos, getUserOrganizations } from "./services/github.service";
-import { generateTopReposHTML } from "./utils/generateTopReposHTML";
+import { ACTIVITY_PLACEHOLDER, DATE_PLACEHOLDER, GITHUB_USERNAME, ORGANIZATIONS_PLACEHOLDER } from './config';
+// import { generateYoutubeHTML } from './utils/generateYoutubeHTML';
+import { getRecentActivity, getUserOrganizations } from "./services/github.service";
+// import { generateTopReposHTML } from "./utils/generateTopReposHTML";
 import { generateRecentActivityHTML } from "./utils/generateRecentActivityHTML";
 import { generateOrganizationsHTML } from "./utils/generateOrganizationHTML";
 
@@ -15,8 +15,8 @@ import { generateOrganizationsHTML } from "./utils/generateOrganizationHTML";
 
     // ---- YOUTUBE ----
 
-    const [repos, activities, organizations] = await Promise.all([
-        getTopStarredRepos(GITHUB_USERNAME),
+    const [activities, organizations] = await Promise.all([
+        // getTopStarredRepos(GITHUB_USERNAME),
         getRecentActivity(GITHUB_USERNAME),
         /* getLatestYoutubeVideos(), */
         getUserOrganizations(GITHUB_USERNAME)
@@ -27,13 +27,12 @@ import { generateOrganizationsHTML } from "./utils/generateOrganizationHTML";
     const newMarkdown = readme
     /* .replace(YOUTUBE_PLACEHOLDER, `\n${latestYoutubeVideos}\n`) */
     .replace(DATE_PLACEHOLDER, format(new Date(), 'dd MMMM yyyy HH:mm'))
-    .replace(STARS_PLACEHOLDER, generateTopReposHTML(repos))
+    //.replace(STARS_PLACEHOLDER, generateTopReposHTML(repos))
     .replace(ACTIVITY_PLACEHOLDER, generateRecentActivityHTML(activities))
     .replace(ORGANIZATIONS_PLACEHOLDER, generateOrganizationsHTML(organizations))
 
 
     console.log('✅ README.md updated with latest information!')
-
 
 
     await fs.writeFile('README.md', newMarkdown)
